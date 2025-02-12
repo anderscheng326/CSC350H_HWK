e6ad0f36b44a084e9898440233d10d481298b2ee
anderscheng326 <anderscheng326@gmail.com>
2025-02-12 13:54:25 -0500

    done - unit 4

--- Program.cs
+++ Program.cs
@@ -1,21 +1,75 @@
 ﻿/**
 Console.WriteLine("start new branch for part3-challenge project");
 
-UNIT 3 - Create and test method
+UNIT 4 - create reuseable methods
 **/
 
+using System;
 
-Console.WriteLine("Generating random numbers: ");
-DisplayRandomNumbers();
+int[] times = {800, 1200, 1600, 2000};
+int diff = 0;
 
-void DisplayRandomNumbers()
+Console.WriteLine("Enter current GMT");
+int currentGMT = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Current Medicine Schedule:");
+
+DisplayTimes();
+
+Console.WriteLine("Enter new GMT");
+int newGMT = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(newGMT) > 12 || Math.Abs(currentGMT) > 12)
 {
-    Random rnd = new Random();
-
-    for (int i = 0; i < 5; i++)
-    {
-        Console.Write($"{rnd.Next(1,100)} ");
-    }
-    Console.WriteLine();
+    Console.WriteLine("Invalid GMT");
+}
+else if (newGMT <= 0 && currentGMT <= 0 || newGMT >= 0 && currentGMT >= 0) 
+{
+    diff = 100 * (Math.Abs(newGMT) - Math.Abs(currentGMT));
+    AdjustTimes(); 
+} 
+else 
+{
+    diff = 100 * (Math.Abs(newGMT) + Math.Abs(currentGMT));
+    AdjustTimes(); 
 }
 
+Console.WriteLine("New Medicine Schedule:");
+
+DisplayTimes();
+
+ void DisplayTimes() 
+{
+    /* Format and display medicine times */
+    foreach (int val in times)
+    {
+        string time = val.ToString();
+        int len = time.Length;
+
+        if (len >= 3)
+        {
+            time = time.Insert(len - 2, ":");
+        }
+        else if (len == 2)
+        {
+            time = time.Insert(0, "0:");
+        }
+        else
+        {
+            time = time.Insert(0, "0:0");
+        }
+
+        Console.Write($"{time} ");
+    }
+
+    Console.WriteLine();
+}   
+
+void AdjustTimes() 
+{
+    /* Adjust the times by adding the difference, keeping the value within 24 hours */
+    for (int i = 0; i < times.Length; i++) 
+    {
+        times[i] = ((times[i] + diff)) % 2400;
+    }
+}
