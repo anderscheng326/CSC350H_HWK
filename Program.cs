cbe5c572ba3d2c2e94007c1eb43f97e12ac7e732
anderscheng326 <anderscheng326@gmail.com>
2025-02-12 13:46:26 -0500

    done - unit 3

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("start new branch for part3-challenge project");
+﻿/**
+Console.WriteLine("start new branch for part3-challenge project");
+
+UNIT 3 - Create and test method
+**/
+
+
+Console.WriteLine("Generating random numbers: ");
+DisplayRandomNumbers();
+
+void DisplayRandomNumbers()
+{
+    Random rnd = new Random();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.Write($"{rnd.Next(1,100)} ");
+    }
+    Console.WriteLine();
+}
+
