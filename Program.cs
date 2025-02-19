f4b5f99d5ec99388d05a80d49f0118fe8561b6ab
anderscheng326 <anderscheng326@gmail.com>
2025-02-19 15:12:02 -0500

    new branch part5-challengeproject

--- Program.cs
+++ Program.cs
@@ -1,39 +1,117 @@
-﻿/**
-Console.WriteLine("start new branch for part3-challenge project");
-
-UNIT 6 - building fortune teller code with methods
-**/
-
+﻿﻿using System;
 
 Random random = new Random();
-int luck = random.Next(100);
-tellFortune(luck);
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
 
-/** unit testing for each
-Console.WriteLine("bad luck");
-int luck1 = 20;
-tellFortune(luck1);
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
 
-Console.WriteLine("good luck");
-int luck2 = 80;
-tellFortune(luck2);
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
 
-Console.WriteLine("neutral luck");
-int luck3 = 50;
-tellFortune(luck3);
-**/
+// Available player and food strings
+string[] states = {"('-')", "(^-^)", "(X_X)"};
+string[] foods = {"@@@@@", "$$$$$", "#####"};
 
-void tellFortune(int luck)
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+InitializeGame();
+while (!shouldExit) 
 {
-string[] text = {"You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to"};
-string[] good = {"look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!"};
-string[] bad = {"fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life."};
-string[] neutral = {"appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature."};
-
-Console.WriteLine("A fortune teller whispers the following words:");
-string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
-for (int i = 0; i < 4; i++) 
-{
-    Console.Write($"{text[i]} {fortune[i]} ");
+    Move();
 }
+
+// Returns true if the Terminal was resized 
+bool TerminalResized() 
+{
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+}
+
+// Displays random food at a random location
+void ShowFood() 
+{
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
+
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer() 
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer() 
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+// Reads directional input from the Console and moves the player
+void Move() 
+{
+    int lastX = playerX;
+    int lastY = playerY;
+    
+    switch (Console.ReadKey(true).Key) 
+    {
+        case ConsoleKey.UpArrow:
+            playerY--; 
+            break;
+		case ConsoleKey.DownArrow: 
+            playerY++; 
+            break;
+		case ConsoleKey.LeftArrow:  
+            playerX--; 
+            break;
+		case ConsoleKey.RightArrow: 
+            playerX++; 
+            break;
+		case ConsoleKey.Escape:     
+            shouldExit = true; 
+            break;
+    }
+
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++) 
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Clears the console, displays the food and player
+void InitializeGame() 
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
 }
