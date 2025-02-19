6128e88a4aad62d57efb88036df3c5a5a2ca96a2
anderscheng326 <anderscheng326@gmail.com>
2025-02-19 14:12:01 -0500

    done - unit3-guided

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿﻿//new branch for part5-guided 
+
+using System;
+
+string[] pettingZoo =
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+//RandomizeAnimals();
+
+//AssignGroup();
+
+Console.WriteLine("School A");
+
+//PrintGroup(group)
+
+//var group = AssignGroup();
+
+//string[,] group = AssignGroup();
